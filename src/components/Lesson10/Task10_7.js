089f2e76fca55abd25bd5943b4cc506c41b7bd8e
tynindima <tynindima@gmail.com>
2020-10-06 12:13:31 +0300

    fall with memo in task 10_7

--- src/components/Lesson10/Task10_7.js
+++ src/components/Lesson10/Task10_7.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 
 import './task10_7.css';
 
@@ -15,51 +15,35 @@ const Task10_7 = () => {
   const products = useProducts(initialProducts);
   const checkets = useCheckeds(initialProducts.map(product => product.name));
 
-  const handleDeleteValue = (name) => {
-    products.deleteValue(name);
-    checkets.deleteValue(name)
-  };
+  const handleDeleteValue = useCallback(
+    (name) => {
+      products.deleteValue(name);
+      checkets.deleteValue(name)
+    },
+  [products, checkets],
+  )
 
   const addNewProduct = (product) => {
     products.addNewValue(product);
     checkets.addNewValue(product.name);
   };
 
-
-  const rowsOfProduct = products.value.map((product, i) => (
-    <Product
-      key={product.name}
-      product={product}
-      number={i}
-      deleteProduct={handleDeleteValue}
-      isChecked={checkets.value[i]}
-      onCheck={checkets.onCheck}
-    />
-  ));
+  console.log('Render Taks10_7');
 
   return (
     <>
-      <table className="table">
-        <thead>
-          <tr>
-            <th className="col">Name</th>
-            <th className="col">Price</th>
-            <th className="col">Count</th>
-            <th className="col">Total Price</th>
-            <th className="col">Delete</th>
-            <th className="col">Checked</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rowsOfProduct}
-        </tbody>
-      </table>
+      <Products
+        products={products}
+        checkets={checkets}
+        onDelete={handleDeleteValue}
+      />
       <AddingProduct addNewProduct={addNewProduct}/>
       <AllCosts products={products.value} checkets={checkets.value}/>
     </>
 
   );
 };
+
 
 const useProducts = (initialState) => {
   const [value, setValue] = useState(initialState);
@@ -68,9 +52,9 @@ const useProducts = (initialState) => {
     setValue([...value, newValue])
   };
 
-  const handlerDeleteValue = (name) => {
-    setValue(value.filter((product) => product.name !== name));
-  };
+  const handlerDeleteValue = useCallback((name) => {
+    setValue(value.filter((product) => product.name !== name))
+  }, [value]);
 
   return {
     value,
@@ -86,19 +70,20 @@ const useCheckeds = (initialState) => {
     setValue([...value, newValue])
   };
 
-  const handlerDeleteValue = (name) => {
+  const handlerDeleteValue = useCallback((name) => {
     setValue(value.filter((product) => product !== name));
-  };
+  }, [value]);
 
-  const handlerCheck = (index, name) => {
+  const handlerCheck = useCallback( (index, name) => {
     setValue(value.map((check, i) => {
       if (i === index) {
         return check ? '' : name;
       }
 
       return check;
-    }));
-  };
+    }))
+    }, [value, setValue]
+  );
 
   return {
     value,
@@ -134,7 +119,44 @@ const useSumbitAddNewProduct = (name, price, count, addProduct) => {
   }
 };
 
-const Product = (props) => {
+const Products = React.memo((props) => {
+  const {
+    products,
+    checkets,
+    onDelete
+  } = props;
+
+  const rowsOfProduct = products.value.map((product, i) => (
+    <Product
+      key={product.name}
+      product={product}
+      number={i}
+      deleteProduct={onDelete}
+      isChecked={checkets.value[i]}
+      onCheck={checkets.onCheck}
+    />
+  ));
+
+  return (
+    <table className="table">
+        <thead>
+          <tr>
+            <th className="col">Name</th>
+            <th className="col">Price</th>
+            <th className="col">Count</th>
+            <th className="col">Total Price</th>
+            <th className="col">Delete</th>
+            <th className="col">Checked</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rowsOfProduct}
+        </tbody>
+      </table>
+  );
+});
+
+const Product = React.memo((props) => {
   const {
     product,
     deleteProduct,
@@ -148,6 +170,7 @@ const Product = (props) => {
     count
   } = product;
 
+  console.log('Render: Product');
   const totalPrice = price * count;
 
   return (
@@ -175,13 +198,15 @@ const Product = (props) => {
       </td>
     </tr>
   );
-};
+});
 
-const Input = (props) => {
+const Input = React.memo((props) => {
   const {
     name,
     inputRef
   } = props;
+
+  console.log('Render: Input');
 
   return (
     <div className="form-box">
@@ -195,9 +220,9 @@ const Input = (props) => {
       />
     </div>
   );
-};
+});
 
-const AddingProduct = (props) => {
+const AddingProduct = React.memo((props) => {
   const { addNewProduct } = props;
 
   const name = useRef();
@@ -205,6 +230,7 @@ const AddingProduct = (props) => {
   const count = useRef();
   const submitNewProduct = useSumbitAddNewProduct(name, price, count, addNewProduct);
 
+  console.log('Render: Adding Product');
   return (
     <form onSubmit={submitNewProduct.onSubmit}>
       <Input
@@ -222,13 +248,14 @@ const AddingProduct = (props) => {
       <button className="btn btn-success" type="submit">Add product</button>
     </form>
   );
-};
+});
 
 const AllCosts = (props) => {
   const {
     products,
     checkets
   } = props;
+  console.log('Render: All Costs');
 
   const totalCosts = products.reduce((acc, product, i) => {
     if (checkets[i]) {
